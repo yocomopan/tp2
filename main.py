abedef549e80b03bf8015ec1556898f4ecd97fa4
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-09-29 16:21:33 -0400

    Added Guessing file and prints

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+2025-09-29
+Ivan Zheryakov
+Guessing the number
+"""
+
+print("Guess the number I'm thinking of")
+# difficulty = input("choose your difficulty, easy medium hard")
+print(f"I've chosen a number between  and ")
+
+
