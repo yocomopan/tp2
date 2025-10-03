d3e87fd8c3c1dfe52e77804ebf6e089768081d91
zheryakovi <zheryakovi@educ.college-montreal.qc.ca>
2025-10-03 15:24:06 -0400

    Modif. attempts variable

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ Guessing the number
 import random
 
 # Variables
-number = random.randint(10, 100)
-
+number = random.randint(10, 11)
+attempts = 0
 """
 def END(exiting_game):
     return exiting_game 
@@ -18,15 +18,21 @@ print("Guess the number I'm thinking of")
 print("I've chosen a number between 10 and 100")
 players_try = float(input("The number you're thinking of is : "))
 if players_try != number:
+    attempts += 1
     print("Nope! Try again.")
+
     while number != players_try:
+        attempts += 1
         players_try = float(input("The number you're thinking of is : "))
         if players_try == number:
             print("Nice Work!")
+            print(f"You did it in {attempts} attempts.")
         else:
             print("Nope!")
+
 else:
     print("Nice work!")
+    print(f"You did it in {attempts} attempts.")
 # input(int("Type 1234 to end"))
 # deciding_to_end = input(int("Type 1234 to end"))
 # END(deciding_to_end)
